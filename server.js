2d0152a8d1d38f052abadf559dc1f38ce6e4928a
Jeff Fav <fav53@yahoo.com>
2016-08-19 11:23:40 -0400

    refactor middleware

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
-
-var middleware = {
-	requireAuthentication: function (req, res, next) {
-		console.log('routeeee');
-		next();
-	},
-	logger: function (req, res, next) {
-		console.log(req.method);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
 
 //app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
