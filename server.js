287fbee95f20798a88d4b12d82471d89cab96d1e
Jeff Fav <fav53@yahoo.com>
2016-08-19 10:47:57 -0400

    Initialize repo

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('route');
+		next();
+	},
+	logger: function (req, res, next) {
+		console.log(req.method);
+		next();
+	}
+};
+
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+app.use(express.static(__dirname + '/public'));
+app.listen(PORT, function () {
+	console.log('server started');
+});
